7a8366598e66ac0d3e3a12893797064d9856990e
kanondev <kanon2017sengrem@gmail.com>
2024-09-24 04:18:52 +0600

    js calculate and donate section done

--- js/calculate.js
+++ js/calculate.js
@@ -30,3 +30,78 @@ else{
 }
 
 })
+
+
+
+
+// 2nd section
+
+document.getElementById('f')
+.addEventListener('click', function (event) {
+    event.preventDefault();
+
+
+    const mainBlance = parseFloat(document.getElementById('main-blance').innerText);
+    const donateBlanceFeni = parseFloat(document.getElementById('donate-2').innerText);
+    console.log(donateBlanceFeni)
+    const feni = parseFloat(document.getElementById("donete-input-2").value);
+    console.log(feni)
+
+//     // 2nd work calculated
+
+
+    if (!isNaN(feni) && feni >= 0) {
+
+    const doneteFeni = mainBlance- feni;
+    const mainBlanceElement = document.getElementById('main-blance')
+    mainBlanceElement.innerText = doneteFeni;
+
+    const doneteAmountFeni = feni + donateBlanceFeni;
+    const donateBlanceElement = document.getElementById('donate-2')
+    donateBlanceElement.innerText = doneteAmountFeni;
+    const modal = document.getElementById('my_modal_1');
+        if (modal) {
+            modal.showModal();
+
+    }
+}
+else{
+    alert('Enter valid Amount');
+}
+
+})
+
+
+// 3rd section
+document.getElementById('aid')
+.addEventListener('click', function (event) {
+    event.preventDefault();
+
+
+    const mainBlance = parseFloat(document.getElementById('main-blance').innerText);
+    const donateBlanceAid = parseFloat(document.getElementById('donate-3').innerText);
+    const aid = parseFloat(document.getElementById("donete-input-3").value);
+
+    // 2nd work calculated
+
+
+    if (!isNaN(aid) && aid >= 0) {
+
+    const doneteaid = mainBlance- aid;
+    const mainBlanceElement = document.getElementById('main-blance')
+    mainBlanceElement.innerText = doneteaid;
+
+    const doneteAmountAid = aid + donateBlanceAid;
+    const donateBlanceElement = document.getElementById('donate-3')
+    donateBlanceElement.innerText = doneteAmountAid;
+    const modal = document.getElementById('my_modal_1');
+        if (modal) {
+            modal.showModal();
+
+    }
+}
+else{
+    alert('Enter valid Amount');
+}
+
+})
