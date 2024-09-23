03f83f13c18f3d89cf6e02c69f58b26a64cdcc15
kanondev <kanon2017sengrem@gmail.com>
2024-09-24 03:53:16 +0600

    dom calculate 1 section done

--- js/calculate.js
+++ js/calculate.js
@@ -1,24 +1,32 @@
 document.getElementById('donate-n')
 .addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('clicked');
 
 
     const mainBlance = parseFloat(document.getElementById('main-blance').innerText);
     const donateBlance = parseFloat(document.getElementById('donate').innerText);
-    console.log(donateBlance)
-    const mohakhali = parseFloat(document.getElementById("donete-input").value);
+    const nohakhali = parseFloat(document.getElementById("donete-input").value);
 
     // 2nd work calculated
 
-    const donete = mainBlance- mohakhali;
-    console.log(donete);
+
+    if (!isNaN(nohakhali) && nohakhali >= 0) {
+
+    const donete = mainBlance- nohakhali;
     const mainBlanceElement = document.getElementById('main-blance')
     mainBlanceElement.innerText = donete;
 
-    const doneteAmount = mohakhali + donateBlance
-    console.log(doneteAmount)
+    const doneteAmount = nohakhali + donateBlance;
     const donateBlanceElement = document.getElementById('donate')
     donateBlanceElement.innerText = doneteAmount;
+    const modal = document.getElementById('my_modal_1');
+        if (modal) {
+            modal.showModal();
+
+    }
+}
+else{
+    alert('Enter valid Amount');
+}
 
 })
